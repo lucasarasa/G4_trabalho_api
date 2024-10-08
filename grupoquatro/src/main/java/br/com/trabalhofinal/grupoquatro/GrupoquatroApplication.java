c7ddc37fc98d06aa9c316738127d104ea5433b87
Lucas Sarasa <lucasmsarasa@gmail.com>
2024-10-07 21:19:43 -0300

    Subindo repositório de grupo

---
+++ grupoquatro/src/main/java/br/com/trabalhofinal/grupoquatro/GrupoquatroApplication.java
@@ -0,0 +1,13 @@
+package br.com.trabalhofinal.grupoquatro;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrupoquatroApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrupoquatroApplication.class, args);
+	}
+
+}
---
+++ grupoquatro/src/test/java/br/com/trabalhofinal/grupoquatro/GrupoquatroApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.trabalhofinal.grupoquatro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrupoquatroApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
