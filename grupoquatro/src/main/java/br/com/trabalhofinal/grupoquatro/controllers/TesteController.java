f2f500295278f07253530288e9061928d7e2d95a
Dgporte <degevg1898@icloud.com>
2024-10-09 15:19:14 -0300

    Teste controller

--- grupoquatro/src/main/java/br/com/trabalhofinal/grupoquatro/controllers/TesteController.java
+++ grupoquatro/src/main/java/br/com/trabalhofinal/grupoquatro/controllers/TesteController.java
@@ -1,5 +1,15 @@
 package br.com.trabalhofinal.grupoquatro.controllers;
 
-public class TesteController {
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
+@RequestMapping("/teste")
+public class TesteController {
+	
+	@GetMapping
+	public String teste() {
+		return "Hello World!";
+	}
 }
