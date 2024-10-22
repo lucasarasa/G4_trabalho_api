9b3f777a0805e25c9b1d68deae0ffea1a5415067
Dgporte <degevg1898@icloud.com>
2024-10-22 18:20:05 -0300

    CRUD Cliente 14.0.0

--- grupoquatro/src/main/java/br/com/trabalhofinal/grupoquatro/configs/SwaggerConfig.java
+++ grupoquatro/src/main/java/br/com/trabalhofinal/grupoquatro/configs/SwaggerConfig.java
@@ -38,7 +38,7 @@ public class SwaggerConfig {
 		
 		Info info = new Info();
 		info.setTitle("Grupo 4");
-		info.setVersion("12.0.0");
+		info.setVersion("14.0.0");
 		info.setDescription("API Grupo 4");
 		info.setContact(contact);
 		info.setLicense(license);
