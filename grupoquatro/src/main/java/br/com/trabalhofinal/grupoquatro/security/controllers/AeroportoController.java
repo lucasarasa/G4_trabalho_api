d3592e9b5888b352c34f2951b5abe85b36c8fc42
Lucas Sarasa <lucasmsarasa@gmail.com>
2024-10-24 16:42:59 -0300

    Subindo permissões

--- grupoquatro/src/main/java/br/com/trabalhofinal/grupoquatro/security/controllers/AeroportoController.java
+++ grupoquatro/src/main/java/br/com/trabalhofinal/grupoquatro/security/controllers/AeroportoController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,7 @@ import br.com.trabalhofinal.grupoquatro.security.entities.Aeroporto;
 import br.com.trabalhofinal.grupoquatro.security.repositories.AeroportoRepository;
 import br.com.trabalhofinal.grupoquatro.security.services.AeroportoService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 
 @RestController
 @RequestMapping("/aeroporto")
@@ -31,12 +33,16 @@ public class AeroportoController {
 	@Autowired
 	AeroportoRepository aeroportoRepository;
 	
+	@SecurityRequirement(name = "Bearer Auth")
+	@PreAuthorize("hasRole('admin')")
 	@GetMapping
 	@Operation(summary="Listar aeroporto")
 	public List<AeroportoRequestDTO> listarAeroporto(){
 		return aeroportoService.listarAeroporto();
 	}
 	
+	@SecurityRequirement(name = "Bearer Auth")
+	@PreAuthorize("hasRole('admin')")
 	@PostMapping
 	@Operation(summary = "Cadastrar aeroporto.")
 	public ResponseEntity<?> cadastrarAeroporto(@RequestBody AeroportoRequestDTO aeroportoRequestDTO){
@@ -51,6 +57,8 @@ public class AeroportoController {
 		
 	}
 	
+	@SecurityRequirement(name = "Bearer Auth")
+	@PreAuthorize("hasRole('admin')")
 	@PutMapping
 	@Operation(summary="Atualizar aeroporto.")
 	public ResponseEntity<?> atualizarAeroporto(@RequestBody AeroportoRequestDTO aeroportoRequestDTO){
@@ -58,6 +66,8 @@ public class AeroportoController {
 		return ResponseEntity.ok(new MessageResponseDTO("Aeroporto atualizado com sucesso!"));
 	}
 	
+	@SecurityRequirement(name = "Bearer Auth")
+	@PreAuthorize("hasRole('admin')")
 	@DeleteMapping("/{id}")
 	@Operation(summary="Deletar aeroporto")
 	public ResponseEntity<String> deletarAeroporto(@PathVariable Integer id){
