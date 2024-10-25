d2ab4b1085a867e897b86e10ba5c96c0bd9c7382
anafortuna <alobofortuna@gmail.com>
2024-10-25 09:22:07 -0300

    Ajuste Post Pedido e versão 25.9.1

--- grupoquatro/src/main/java/br/com/trabalhofinal/grupoquatro/security/repositories/ProdutoRepository.java
+++ grupoquatro/src/main/java/br/com/trabalhofinal/grupoquatro/security/repositories/ProdutoRepository.java
@@ -1,10 +1,15 @@
 package br.com.trabalhofinal.grupoquatro.security.repositories;
 
+import java.util.Set;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import br.com.trabalhofinal.grupoquatro.security.entities.Produto;
 
 
 public interface ProdutoRepository extends JpaRepository<Produto, Integer>  {
 
+	@Query(value = "select * from produto where pro_cd_id in (:ids)", nativeQuery = true)
+	public Set<Produto> retornaLista(Set<Integer> ids);
 }
